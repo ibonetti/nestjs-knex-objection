dca4f2bd6feb15db75fd4ed01d2a557ee5a2aecd
ibonetti <israelbonetti@gmail.com>
2022-09-15 10:24:00 -0300

    refactoring

--- src/messaging/messaging.service.ts
+++ src/messaging/messaging.service.ts
@@ -1,13 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { NotifyNewTaskDto } from './dto/notify-newtask.dto';
 import { Twilio } from 'twilio';
 import { ConfigService } from '@nestjs/config';
+import { MessageInstance } from 'twilio/lib/rest/api/v2010/account/message';
 
 @Injectable()
 export class MessagingService {
+  private logger = new Logger('MessagingService');
   constructor(private config: ConfigService) {}
 
-  sendSms(message: string, phoneNumber: string) {
+  async sendSms(
+    message: string,
+    phoneNumber: string,
+  ): Promise<MessageInstance> {
     const accountSid = this.config.get('TWILIO_ACCOUNT_SID');
     const authToken = this.config.get('TWILIO_AUTH_TOKEN');
 
@@ -15,21 +20,20 @@ export class MessagingService {
 
     const client = new Twilio(accountSid, authToken);
 
-    client.messages
-      .create({
-        body: message,
-        from: '+12564083682',
-        to: phoneNumber,
-      })
-      .then((message) => console.log(message.sid))
-      .catch((err) => console.log(err));
+    return await client.messages.create({
+      body: message,
+      from: '+12564083682', //'+12564083682'
+      to: phoneNumber,
+    });
+    //.then((message) => console.log(message.sid))
+    //.catch((err) => console.log(err));
   }
 
   sendNewtaskSmsToUser(notifyDto: NotifyNewTaskDto) {
     const { user, task } = notifyDto;
 
     const msg = `Hi ${user.username}. You've got a new task "${task.title}": ${task.description}`;
-    console.log(`SMS ==> ${msg}`);
+    this.logger.debug(`SMS ==> ${msg}`);
 
     this.sendSms(msg, user.phone_number);
   }
--- src/tasks/sms.processor.ts
+++ src/tasks/sms.processor.ts
@@ -1,29 +1,46 @@
-import { Processor, Process, OnQueueActive, OnQueueError } from '@nestjs/bull';
+import {
+  Processor,
+  Process,
+  OnQueueActive,
+  OnQueueError,
+  OnQueueFailed,
+} from '@nestjs/bull';
+import { Logger } from '@nestjs/common';
 import { Job } from 'bull';
+import { MessageInstance } from 'twilio/lib/rest/api/v2010/account/message';
 import { MessagingService } from '../messaging/messaging.service';
 import { TaskSmsInterface } from './dto/task-sms.interface';
 
 @Processor('sms')
 export class SmsConsumer {
+  private logger = new Logger('SmsConsumer');
   constructor(private messagingService: MessagingService) {}
 
   @Process('tasksms')
-  async sendSms(job: Job<TaskSmsInterface>) {
+  async sendSms(job: Job<TaskSmsInterface>): Promise<MessageInstance> {
     const { username, taskTitle, taskDescription, phoneNumber } = job.data;
 
     const msg = `Hi ${username}. You've got a new task "${taskTitle}": ${taskDescription}`;
-    Promise.resolve(this.messagingService.sendSms(msg, phoneNumber));
+
+    return this.messagingService.sendSms(msg, phoneNumber);
   }
 
   @OnQueueActive()
   onActive(job: Job) {
-    console.log(
-      `Processing job ${job.id} of type ${job.name} with data ${job.data}...`,
+    this.logger.verbose(
+      `Processing job ${job.id} of type ${job.name} with data ${JSON.stringify(
+        job.data,
+      )}...`,
     );
   }
 
   @OnQueueError()
   onError(error: Error) {
-    console.log(error.message);
+    this.logger.error(`SMS queue error: ${error.message}`);
+  }
+
+  @OnQueueFailed()
+  onFailed(job: Job, error: Error) {
+    this.logger.error(`Job ${job.id} failed. Reason: ${error.message}`);
   }
 }
